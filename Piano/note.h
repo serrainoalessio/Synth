04c2263fe52571a905801100f1a4053e3fa58795
serrainoalessio <serrainoalessio@gmail.com>
2014-11-19 21:47:03 +0100

    Create note.h

---
+++ Piano/note.h
@@ -0,0 +1,95 @@
+#ifndef NOTE_HEADER_GUARD
+#define NOTE_HEADER_GUARD
+
+#define NOTE_Do1    12
+#define NOTE_Reb1   13
+#define NOTE_Re1    14
+#define NOTE_Mib1   15
+#define NOTE_Mi1    16
+#define NOTE_Fa1    17
+#define NOTE_Solb1  18
+#define NOTE_Sol1   19
+#define NOTE_Lab1   20
+#define NOTE_La1    21
+#define NOTE_Sib1   22
+#define NOTE_Si1    23
+
+#define NOTE_Do2    24
+#define NOTE_Reb2   25
+#define NOTE_Re2    26
+#define NOTE_Mib2   27
+#define NOTE_Mi2    28
+#define NOTE_Fa2    29
+#define NOTE_Solb2  30
+#define NOTE_Sol2   31
+#define NOTE_Lab2   32
+#define NOTE_La2    33
+#define NOTE_Sib2   34
+#define NOTE_Si2    35
+
+#define NOTE_Do3    36
+#define NOTE_Reb3   37
+#define NOTE_Re3    38
+#define NOTE_Mib3   39
+#define NOTE_Mi3    40
+#define NOTE_Fa3    41
+#define NOTE_Solb3  42
+#define NOTE_Sol3   43
+#define NOTE_Lab3   44
+#define NOTE_La3    45
+#define NOTE_Sib3   46
+#define NOTE_Si3    47
+
+#define NOTE_Do4    48
+#define NOTE_Reb4   49
+#define NOTE_Re4    50
+#define NOTE_Mib4   51
+#define NOTE_Mi4    52
+#define NOTE_Fa4    53
+#define NOTE_Solb4  54
+#define NOTE_Sol4   55
+#define NOTE_Lab4   56
+#define NOTE_La4    57
+#define NOTE_Sib4   58
+#define NOTE_Si4    59
+
+#define NOTE_Do5    60
+#define NOTE_Reb5   61
+#define NOTE_Re5    62
+#define NOTE_Mib5   63
+#define NOTE_Mi5    64
+#define NOTE_Fa5    65
+#define NOTE_Solb5  66
+#define NOTE_Sol5   67
+#define NOTE_Lab5   68
+#define NOTE_La5    69
+#define NOTE_Sib5   70
+#define NOTE_Si5    71
+
+#define NOTE_Do6    72
+#define NOTE_Reb6   73
+#define NOTE_Re6    74
+#define NOTE_Mib6   75
+#define NOTE_Mi6    76
+#define NOTE_Fa6    77
+#define NOTE_Solb6  78
+#define NOTE_Sol6   79
+#define NOTE_Lab6   80
+#define NOTE_La6    81
+#define NOTE_Sib6   82
+#define NOTE_Si6    83
+
+#define NOTE_Do7    84
+#define NOTE_Reb7   85
+#define NOTE_Re7    86
+#define NOTE_Mib7   87
+#define NOTE_Mi7    88
+#define NOTE_Fa7    89
+#define NOTE_Solb7  90
+#define NOTE_Sol7   91
+#define NOTE_Lab7   92
+#define NOTE_La7    93
+#define NOTE_Sib7   94
+#define NOTE_Si7    95
+
+#endif // NOTE_HEADER_GUARD defined

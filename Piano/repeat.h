ae867174edf1e3f1e349892c1d2fda274a7a2101
serrainoalessio <serrainoalessio@gmail.com>
2014-11-19 21:47:55 +0100

    Create repeat.h

---
+++ Piano/repeat.h
@@ -0,0 +1,34 @@
+#ifndef NOP_H_HEADER_GUARD
+#define NOP_H_HEADER_GUARD
+
+
+#define  _0(x)
+#define  _1(x) x  _0(x)
+#define  _2(x) x  _1(x)
+#define  _3(x) x  _2(x)
+#define  _4(x) x  _3(x)
+#define  _5(x) x  _4(x)
+#define  _6(x) x  _5(x)
+#define  _7(x) x  _6(x)
+#define  _8(x) x  _7(x)
+#define  _9(x) x  _8(x)
+#define _10(x) x  _9(x)
+#define _11(x) x _10(x)
+#define _12(x) x _11(x)
+#define _13(x) x _12(x)
+#define _14(x) x _13(x)
+#define _15(x) x _14(x)
+#define _16(x) x _15(x)
+#define _17(x) x _16(x)
+#define _18(x) x _17(x)
+#define _19(x) x _18(x)
+#define _20(x) x _19(x)
+#define _21(x) x _20(x)
+#define _22(x) x _21(x)
+#define _23(x) x _22(x)
+#define _24(x) x _23(x)
+#define _25(x) x _24(x)
+// ...
+// it should go on up to infinity
+
+#endif
